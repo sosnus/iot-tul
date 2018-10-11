8117c102ba9ed1628b86f94efa805685252cf98f
Sosnus <sosnus15@gmail.com>
2018-10-11 12:07:32 +0200

    added proj to read data from AZURE DB

---
+++ src/sqltest/SensorData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Data.SqlClient;
+using System.Text;
+
+
+class SensorData
+{
+    public int ID;
+    public int SensorId;
+    public String Description;
+    public int Value;
+}
