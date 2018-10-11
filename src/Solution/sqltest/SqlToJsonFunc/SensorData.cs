6cf0b5eb2dac08fc5c7b9d1caccfdadade57b8b5
Sosnus <sosnus15@gmail.com>
2018-10-11 13:32:33 +0200

    func working local, but without sql select

---
+++ src/Solution/sqltest/SqlToJsonFunc/SensorData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+
+class SensorData
+{
+    public int ID;
+    public int SensorId;
+    public String Description;
+    public int Value;
+}
