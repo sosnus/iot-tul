7ea6770c50098494d7d01088a21da5d0ec64bbc9
Sosnus <sosnus15@gmail.com>
2018-10-11 12:19:18 +0200

    new solution

---
+++ src/Solution/sqltest/sqltest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
