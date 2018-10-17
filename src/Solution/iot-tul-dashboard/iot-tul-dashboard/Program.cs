6d77123b2bcb2f70d5bb81f25959cb5157c85927
Sosnus <sosnus15@gmail.com>
2018-10-17 16:36:06 +0200

    old program to new solution

--- src/Solution/iot-tul-dashboard/iot-tul-dashboard/Program.cs
+++ src/Solution/iot-tul-dashboard/iot-tul-dashboard/Program.cs
@@ -4,9 +4,58 @@ namespace iot_tul_dashboard
 {
     class Program
     {
+
+        const string sqlQuery = "SELECT * FROM [dbo].[measurementsLog]";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "iot-tul-dashboard-db.database.windows.net";
+                builder.UserID = "tul-admin";
+                builder.Password = "22DQqNdU";
+                builder.InitialCatalog = "iot-tul-dashboard-db";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nExecute {0} ...", sqlQuery);
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(sqlQuery);
+                    String sql = sb.ToString();
+
+                    Console.WriteLine("\nWrite data to collection...");
+                    string json = null;
+                    List<measurement> kolekcja = new List<measurement>();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                measurement odczyt = new measurement();
+                                odczyt.idMeas = reader.GetInt32(0);
+                                odczyt.idSensor = reader.GetInt32(1);
+                                odczyt.dateMeas = reader.GetDateTime(2);
+                                odczyt.valueMeas = reader.GetFloat(3);
+                                kolekcja.Add(odczyt);
+                            }
+                        }
+
+                    }
+                    Console.WriteLine("\nRewrite collection to json string...\n");
+                    json = JsonConvert.SerializeObject(kolekcja);
+                    Console.WriteLine(json);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nPress any key to exit...\n");
+
+            Console.ReadKey();
         }
     }
 }
