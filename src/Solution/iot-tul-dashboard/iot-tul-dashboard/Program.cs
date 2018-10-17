566f9863a023b3d7b289254c8b686ddf11a59bf4
Sosnus <sosnus15@gmail.com>
2018-10-17 16:31:48 +0200

    new solution

---
+++ src/Solution/iot-tul-dashboard/iot-tul-dashboard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iot_tul_dashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
