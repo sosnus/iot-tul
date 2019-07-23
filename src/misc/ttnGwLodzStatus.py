479329faaa9322bd5466dd86ff03972157a7bf89
Sosnus <sosnus15@gmail.com>
2019-07-23 16:21:30 +0200

    Create ttnGwLodzStatus.py

---
+++ src/misc/ttnGwLodzStatus.py
@@ -0,0 +1,22 @@
+print(">>> SOSNUS SCRIPT ( ttnGwLodzStatus.py ): TTN GW in Lodz, owners and last seen <<<")
+print("Create: 16:14 23-Jul-19 last modif: 16:14 23-Jul-19")
+import requests
+import json
+r =  requests.get('https://www.thethingsnetwork.org/gateway-data/location?latitude=51.76873234&longitude=19.4569911&distance=50000')
+r = r.json()
+
+for x in r:
+  print('NAME: ',x)
+  print('     OWNER:',r[x].get('owner'))
+#  print('     OWNERS:',r[x].get('owners'))
+  print('     DESC:',r[x].get('description'))
+  print('     LAST SEEN:',r[x].get('last_seen'))
+  from datetime import datetime
+  strTime = r[x].get('last_seen')
+  #example strTime input: '2019-07-23T12:49:36Z'
+  time = datetime.strptime(strTime, "%Y-%m-%dT%H:%M:%SZ")
+  time2 = datetime.now()
+  delta = time2 - time
+  print('     LAST SEEN DELTA:',delta)
+  
+  # try code: https://repl.it/repls/SeparateScientificRoutine
