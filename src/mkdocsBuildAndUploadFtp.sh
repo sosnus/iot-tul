824b5c9b1e2d378d04732a750c25f1630341c682
Sosnus <sosnus15@gmail.com>
2019-10-09 17:33:07 +0200

    Update mkdocsBuildAndUploadFtp.sh

--- src/mkdocsBuildAndUploadFtp.sh
+++ src/mkdocsBuildAndUploadFtp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+figlet B and U
 echo "mkdocs build"
 mkdocs build
 echo "upload"
 ncftpput -R -v -u "ftpc52_iottul" ftp.iot-tul.p.lodz.pl /web/ ./site/*
 date +"%D %T"
+echo "============ mkdocsBuildAndUploadFtp END ! ===================="
